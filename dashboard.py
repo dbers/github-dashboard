9949c648b572edf86442cda757b8681cb3ab07d8
Daniel Boland <dan@danboland.com>
2019-06-21 17:11:46 -0400

    fixing typos to have static paths work

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path="static")
+app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def index():
     """Show the main dashboard"""
-    return render_template('index.html', name=name)
-
-
-@app.route('/githup/<organization>')
-def show_user_profile(organization):
-    # show the user profile for that user
-    return 'Get info for %s' % organization
+    return render_template('index.html')
 
 @app.route('/githup/<organization>')
-def show_user_profile(organization):
-    # show the user profile for that user
+def get_orgs_github_data(organization):
+    """Get the an organizations github information
+    
+    Arguments:
+        name (string) -- Name of organization
+        
+    Return: Json data
+    """
     return 'Get info for %s' % organization
